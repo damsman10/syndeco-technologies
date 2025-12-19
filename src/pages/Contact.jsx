22f62d097d6a40202da31890c270ce848a719652
damsman10 <damilolaoni07@gmail.com>
2025-12-19 16:31:42 +0100

    motion uncommented

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,5 @@
 import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
-// import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 const Contact = () => {
   return (
